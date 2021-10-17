92ae6aaf763dc4dc55fd0b1b3d2442de343f2561
imarunkumar <imarunkumar.ksrct@gmail.com>
2021-10-17 17:25:37 +0530

    loader implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,25 @@ import { AppComponent } from './app.component';
 import { CommonComponentModule } from './common-module/common-component.module';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxUiLoaderHttpModule, NgxUiLoaderModule, NgxUiLoaderRouterModule } from 'ngx-ui-loader';
 
+import {
+  NgxUiLoaderConfig,
+  SPINNER,
+  POSITION,
+  PB_DIRECTION,
+} from "ngx-ui-loader";
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  bgsColor: "red",
+  bgsPosition: POSITION.bottomCenter,
+  bgsSize: 40,
+  // fgsColor: "#5abe67",
+  bgsType: SPINNER.rectangleBounce, // background spinner type
+  fgsType: SPINNER.rotatingPlane, // foreground spinner type
+  pbDirection: PB_DIRECTION.leftToRight, // progress bar direction
+  pbThickness: 5, // progress bar thickness
+};
 @NgModule({
   declarations: [
     AppComponent
@@ -16,6 +34,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     CommonComponentModule,
     BrowserAnimationsModule,
+    NgxUiLoaderModule, // import NgxUiLoaderModule
+    NgxUiLoaderHttpModule,
+    NgxUiLoaderRouterModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig),
     ToastrModule.forRoot()
   ],
   providers: [],
--- src/app/common-module/signup/signup.component.ts
+++ src/app/common-module/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { NgForm } from '@angular/forms';
 import { LocalStorageServiceService } from 'src/app/service/local-storage-service.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-signup',
@@ -9,7 +10,7 @@ import { LocalStorageServiceService } from 'src/app/service/local-storage-servic
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  constructor(private toaster:ToastrService,private addUser:LocalStorageServiceService) {
+  constructor(private toaster:ToastrService,private addUser:LocalStorageServiceService,private ngxLoader:NgxUiLoaderService) {
     document.body.style.backgroundImage ="url('/assets/bg-image/loginBg.jpg')";
    }
 
@@ -18,14 +19,21 @@ export class SignupComponent implements OnInit {
   }
 
   signIn(form: NgForm){
-    
+    this.ngxLoader.start();
     if(form.form.status==="VALID"){
-      this.addUser.setData(form.value['email'],JSON.stringify(form.value));
-      this.toaster.success("Successfully Submitted.");
+      console.log(this.addUser.getData('email'));
+      
+      if(this.addUser.getData(form.value['email'])){
+        this.toaster.warning("This user already has an account");
+      }else{
+        this.addUser.setData(form.value['email'],JSON.stringify(form.value));
+        this.toaster.success("Successfully Submitted.");
+      }
     }else{
       this.toaster.error("Invalid form");
 
     }
+    this.ngxLoader.stop();
   }
   
 }
