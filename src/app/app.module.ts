9881c485de7193adf6c96ecc7615434399be6742
imarunkumar <imarunkumar.ksrct@gmail.com>
2021-10-17 15:20:57 +0530

    ngx-toaster added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonComponentModule } from './common-module/common-component.module';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { CommonComponentModule } from './common-module/common-component.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonComponentModule
+    CommonComponentModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
