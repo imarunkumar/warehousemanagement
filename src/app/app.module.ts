3f13ddf4206025b36babd6009c29f129589e02b6
imarunkumar <imarunkumar.ksrct@gmail.com>
2021-10-18 19:44:52 +0530

    font awesome icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {
   POSITION,
   PB_DIRECTION,
 } from "ngx-ui-loader";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 // import { StockModule } from './module/stock-module';
 
@@ -33,6 +34,7 @@ const ngxUiLoaderConfig: NgxUiLoaderConfig = {
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
     AppRoutingModule,
     CommonComponentModule,
     BrowserAnimationsModule,
