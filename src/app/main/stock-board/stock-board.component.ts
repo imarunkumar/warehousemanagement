ea43d62fceab3ed8e89c4107bee33c0fce7691af
imarunkumar <imarunkumar.ksrct@gmail.com>
2021-10-22 21:28:59 +0530

    Edit table funtionality

--- src/app/main/stock-board/stock-board.component.ts
+++ src/app/main/stock-board/stock-board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import * as XLSX from 'xlsx';
 
@@ -14,31 +14,31 @@ export class StockBoardComponent implements OnInit {
         "location":"Chennai",
         "avaliable":120,
         "booked":10,
-        "price":"$125.22"
+        "price":"125.22"
     },{
         "stockName":"ESD",
         "location":"Pune",
         "avaliable":220,
         "booked":134,
-        "price":"$4546.265"
+        "price":"4546.265"
     },{
         "stockName":"CGI",
         "location":"Chennai",
         "avaliable":3420,
         "booked":1035,
-        "price":"$1232.2"
+        "price":"1232.2"
     },{
         "stockName":"RRT",
         "location":"Kerala",
         "avaliable":435,
         "booked":40,
-        "price":"$1232.23"
+        "price":"1232.23"
     },{
         "stockName":"UQI",
         "location":"Bangalore",
         "avaliable":1320,
         "booked":110,
-        "price":"$455.23"
+        "price":"455.23"
     }
     
     ];
@@ -53,9 +53,13 @@ export class StockBoardComponent implements OnInit {
   isAddStock:boolean=false;
   stockName:String;
   willDownload = false;
-  constructor(private toaster:ToastrService) { }
+  isEditStock:boolean=false;
+  editIndex;
+  constructor(private toaster:ToastrService,private eleRef:ElementRef) { }
 
   ngOnInit(): void {
+    console.log(this.data);
+    
   }
   addStock(){
     this.isAddStock=!this.isAddStock
@@ -116,5 +120,22 @@ export class StockBoardComponent implements OnInit {
       );
       el.setAttribute('download', 'xlsxtojson.json');
     }, 1000);
+  }
+
+  editStock(index){
+    this.isEditStock=!this.isEditStock;
+    if (this.isEditStock) {
+      this.editIndex=index;
+      
+    } else {
+      this.editIndex=null
+    }
+
+  }
+
+  deleteStock(index){
+    this.data.splice(index,1)
+    console.log(this.data);
+    
   }
 }
